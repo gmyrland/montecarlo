f5da8407df47a751cd9697c3e2b4c5b2689bd6d1
Glen Myrland <glen@myrland.com>
2017-03-22 13:56:02 -0400

    Build basic UI

--- app.R
+++ app.R
@@ -1,38 +1,48 @@
 library(shiny)
 
+## Source the Monte Carlo functions
+source("R/mcfuncs2.R", local=TRUE)
+
+## Temporary defaults
+default_global <- paste0("uniform <- function(min, max) runif(n=1, min=min, max=max)",
+                        "\nnormal <- function(sd, mean) rnorm(n=1, sd=sd, mean=mean)")
+default_init   <- "x <- uniform(min=5, max=10)\ny <- normal(sd=0.5, mean=0)"
+default_expr   <- "x + y"
+default_n      <- 1000
+
 ## Shiny UI
 ui <- fluidPage(
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Monte Carlo"),
 
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      textInput("n", "Number of runs:", default_n),
+      actionButton("simulate", "Run Simulation")
     ),
 
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      textAreaInput("global", "Global", default_global),
+      textAreaInput("init", "Run Initialization", default_init),
+      textAreaInput("expr", "Expression", default_expr),
+      plotOutput("result")
     )
   )
 )
 
 ## Shiny Server
 server <- function(input, output, session) {
-  output$distPlot <- renderPlot({
+  results <- eventReactive(input$simulate, {
+      # Run simulation
+      n <- input$n
+      global <- input$global
+      init <- input$init
+      expr <- input$expr
+      run_monte_carlo(n, global, init, expr)
+  })
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  output$result <- renderPlot({
+    hist(results()$result)
   })
 
   session$onSessionEnded(function() stopApp(returnValue=NULL))

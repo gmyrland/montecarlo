ab16e18a7f93c3d6a66903be5dd4515782df7bcb
Glen Myrland <glenmyrland@gmail.com>
2017-03-21 21:30:13 -0400

    Convert to single app.R file

---
+++ app.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+## Shiny UI
+ui <- fluidPage(
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+## Shiny Server
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+  })
+
+  session$onSessionEnded(function() stopApp(returnValue=NULL))
+}
+
+shinyApp(ui, server)
